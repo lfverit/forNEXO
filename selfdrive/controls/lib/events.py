85e4b1cb71b443c66b67c0577195c11e9d3319f4
gomtings <33356618+gomtings@users.noreply.github.com>
2020-11-16 23:12:07 +0900

    cencle_btn_인게이지 풀림

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -503,9 +503,9 @@ EVENTS: Dict[int, Dict[str, Union[Alert, Callable[[Any, messaging.SubMaster, boo
     ET.USER_DISABLE: EngagementAlert(AudibleAlert.chimeDisengage),
   },
 
-  EventName.buttonCancel: {
-    ET.USER_DISABLE: EngagementAlert(AudibleAlert.chimeDisengage),
-  },
+  #EventName.buttonCancel: {
+   # ET.USER_DISABLE: EngagementAlert(AudibleAlert.chimeDisengage),
+  #},
 
   EventName.brakeHold: {
     ET.USER_DISABLE: EngagementAlert(AudibleAlert.chimeDisengage),
